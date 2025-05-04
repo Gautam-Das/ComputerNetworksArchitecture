c2cef94d347b416f43289a2ae85ef73ef2b8b74e
Gautam Das <gdas2005gdas@gmail.com>
2025-05-04 12:34:49 +0930

    create header file for selective repeat

---
+++ sr.h
@@ -0,0 +1,6 @@
+extern void A_init(void);
+extern void B_init(void);
+extern void A_input(struct pkt);
+extern void B_input(struct pkt);
+extern void A_output(struct msg);
+extern void A_timerinterrupt(void);
