d6ed057f537c43b6f40dcb7647f7ef40fb020f5a
Gautam Das <gdas2005gdas@gmail.com>
2025-05-04 15:13:59 +0930

    missing definitions

--- sr.h
+++ sr.h
@@ -4,3 +4,7 @@ extern void A_input(struct pkt);
 extern void B_input(struct pkt);
 extern void A_output(struct msg);
 extern void A_timerinterrupt(void);
+
+#define BIDIRECTIONAL 0 
+extern void B_output(struct msg);
+extern void B_timerinterrupt(void);
